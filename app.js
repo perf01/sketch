232a6666f3165be7e50921b55784560efb785bac
perf <electronicfan@proton.me>
2025-03-01 12:35:25 +0100

    add createSquare function

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log(true);
+const createSquares = (width, height, color, quantity) => {
+  const squares = [];
+  const square = document.createElement('div');
+  for (let i = 1; i <= quantity; i++) {
+    const square = document.createElement('div');
+    square.style.width = `${width}px`;
+    square.style.height = `${height}px`;
+    square.style.backgroundColor = color;
+    squares.push(square);
+  }
+  return squares;
+};
