c357e446ab19279e2697ab3f0b71e6d0eaddc183
perf <electronicfan@proton.me>
2025-03-02 20:46:59 +0100

    add random square colors feature

--- app.js
+++ app.js
@@ -2,12 +2,20 @@
   const container = document.querySelector('.container');
   const resizeBtn = document.querySelector('.resize');
 
+  const getRandomColor = () => {
+    const values = [];
+    for (let i = 0; i < 3; i++) {
+      const randomNumber = Math.floor(Math.random() * 256);
+      values.push(randomNumber);
+    }
+    return values;
+  };
   const createSquares = (quantity = 16) => {
     const squares = [];
     for (let i = 1; i <= quantity * quantity; i++) {
       const square = document.createElement('div');
-      square.style.width = `${960 / quantity}px`;
-      square.style.height = `${960 / quantity}px`;
+      square.style.width = `${660 / quantity}px`;
+      square.style.height = `${660 / quantity}px`;
       square.style.opacity = 0.2;
       squares.push(square);
     }
@@ -15,9 +23,13 @@
   };
 
   const createHoverEffect = (event) => {
-    event.target.style.backgroundColor = 'lightcoral';
-    console.log(event.target.style.opacity);
-    event.target.style.opacity = +event.target.style.opacity + 0.1;
+    const random = getRandomColor();
+    if (event.target.style.backgroundColor) {
+      event.target.style.opacity = +event.target.style.opacity + 0.1;
+    } else {
+      event.target.style.background = `RGB(${random.toString()})`;
+      event.target.style.opacity = +event.target.style.opacity + 0.1;
+    }
   };
 
   const appendSquares = (size = 16) => {
