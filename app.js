b93da4ca7e21802bea3b9ba03c7a7e8bcf38b303
perf <electronicfan@proton.me>
2025-03-02 16:28:00 +0100

    add IIFE/resizeGread function

--- app.js
+++ app.js
@@ -1,26 +1,36 @@
-const createSquares = (width, height, color, quantity) => {
-  const squares = [];
-  for (let i = 1; i <= quantity; i++) {
-    const square = document.createElement('div');
-    square.style.width = `${width}px`;
-    square.style.height = `${height}px`;
-    square.style.backgroundColor = color;
-    squares.push(square);
-  }
-  return squares;
-};
-
-const createHoverEffect = (event) => {
-  event.target.style.backgroundColor = 'lightcoral';
-};
-
-const appendSquares = () => {
+(() => {
   const container = document.querySelector('.container');
-  const squares = createSquares('60', '60', 'lightblue', 256);
-  squares.forEach((square) => container.appendChild(square));
-  squares.forEach((square) =>
-    square.addEventListener('mouseover', (event) => createHoverEffect(event))
-  );
-};
+  const resizeBtn = document.querySelector('.resize');
 
-appendSquares();
+  const createSquares = (quantity = 16) => {
+    const squares = [];
+    const realQuantity = quantity * quantity;
+    for (let i = 1; i <= quantity * quantity; i++) {
+      const square = document.createElement('div');
+      square.style.width = `${960 / quantity}px`;
+      square.style.height = `${960 / quantity}px`;
+      squares.push(square);
+    }
+    return squares;
+  };
+
+  const createHoverEffect = (event) => {
+    event.target.style.backgroundColor = 'lightcoral';
+  };
+
+  const appendSquares = (size = 16) => {
+    const squares = createSquares(size);
+    squares.forEach((square) => container.appendChild(square));
+    squares.forEach((square) =>
+      square.addEventListener('mouseover', (event) => createHoverEffect(event))
+    );
+  };
+  const resizeGread = () => {
+    const size = prompt('Enter new size');
+    container.innerHTML = '';
+    appendSquares(size);
+  };
+  resizeBtn.addEventListener('click', resizeGread);
+
+  appendSquares(16);
+})();
